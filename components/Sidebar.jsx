36ace5eda8dde695fb5de4fb2ec13b7c1752cc63
Lázaro Vanderson <eulazzo@protonmail.com>
2022-04-04 20:42:38 -0300

    tailwind utilities, componentes and global css style

---
+++ components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import { SideBarLink } from ".";
+
+const Sidebar = () => {
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full ">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+      </div>
+
+      <div className="space-y-2.5 mt-4 mb-2.5">
+        <SideBarLink />
+      </div>
+
+    </div>
+  );
+};
+
+export default Sidebar;
