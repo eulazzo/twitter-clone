c537d0948b357b715b62e6691e46fc2ec44c7302
Lázaro Vanderson <eulazzo@protonmail.com>
2022-04-09 22:06:02 -0300

    feed topbar

--- components/index.js
+++ components/index.js
@@ -1,2 +1,4 @@
 export { default as Sidebar } from "./Sidebar";
 export { default as SideBarLink } from "./SideBarLink";
+export { default as Feed } from "./Feed";
+export { default as Input } from "./Input";
